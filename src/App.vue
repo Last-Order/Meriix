64a08a97e74bc1833f9211c0ad2691845c366b25
Eridanus Sora <sora@sound.moe>
2019-10-03 10:34:35 +0800

    add vue-cli config

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <v-app>
+    <v-tabs
+      v-model="active"
+      color="cyan"
+      dark
+      slider-color="yellow"
+    >
+      <v-tab
+        key="videoEncode"
+        ripple
+      >
+        视频编码
+      </v-tab>
+      <v-tab-item
+        key="videoEncode"
+      >
+        <v-card flat>
+          <video-encode />
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
   </v-app>
 </template>
 
 <script>
-
+import VideoEncode from '@/components/VideoEncode/Index';
 export default {
   name: 'App',
   data: () => ({
-    //
+    active: 0
   }),
+  components: {
+    VideoEncode
+  }
 };
 </script>
---
+++ vue.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+module.exports = {
+    devServer: {
+        host: 'localhost',
+        disableHostCheck: true
+    },
+    transpileDependencies: [
+        
+    ],
+    chainWebpack: (config)=>{
+        config.resolve.alias
+            .set('@', path.resolve('src'))
+    },
+    pluginOptions: {
+        electronBuilder: {
+            chainWebpackMainProcess: config => {
+                // Chain webpack config for electron main process only
+                config.mode('development');
+            },
+            externals: [],
+            builderOptions: {
+                appId: 'moe.sound.sora.meriix',
+                // asar: false,
+                artifactName: "meriix-${os}-${version}.${ext}",
+                productName: 'Meriix',
+                win: {
+                    target: ['msi'],
+                    icon: 'build/icons/icon.ico',
+                },
+                nsis: {
+                    oneClick: false,
+                    allowToChangeInstallationDirectory: true
+                },
+            }
+        }
+    }
+}
