d4b477e237a68a2070f64a920cdbb35558e7e97f
Eridanus Sora <sora@sound.moe>
2021-03-11 00:15:13 +0800

    new task steps

--- src/components/TaskQueue/TaskSteps.vue
+++ src/components/TaskQueue/TaskSteps.vue
@@ -1,28 +1,74 @@
 <template>
-    <div>
-        <v-stepper vertical>
-            <template v-for="(step, index) in steps">
-                <v-stepper-step :complete="current > index" :step="index" :key="step.uuid">
-                    <div class="step-name">{{ step.stepName || getStepTypeName(step.type) }}</div>
+    <div class="task-steps-container">
+        <template v-for="(step, index) in steps">
+            <div class="step-item" :key="step.uuid" :class="{ upcoming: current <= index }">
+                <div class="left">
+                    <v-icon class="status-icon" color="#3f51b5" v-if="current > index"
+                        >mdi-check</v-icon
+                    >
+                    <v-icon class="status-icon loading" color="#3f51b5" v-if="current === index"
+                        >mdi-loading</v-icon
+                    >
+                    <v-icon class="status-icon" color="#3f51b5" v-if="current < index"
+                        >mdi-progress-clock</v-icon
+                    >
+                </div>
+                <div class="right">
+                    <div class="step-type">{{ step.type && getStepTypeName(step.type) }}</div>
+                    <div class="step-name">{{ step.stepName }}</div>
                     <v-progress-linear
                         v-if="task.progress !== undefined && current == index"
                         :value="task.progress"
                     ></v-progress-linear>
-                </v-stepper-step>
-            </template>
-        </v-stepper>
+                </div>
+            </div>
+        </template>
     </div>
 </template>
 <style lang="scss">
-.v-stepper {
-    box-shadow: none;
+@keyframes spin {
+    from {
+        transform: rotate(0deg);
+    }
+    to {
+        transform: rotate(360deg);
+    }
 }
-.v-stepper__label {
+.loading {
+    animation: 1s spin 0s infinite;
+}
+.task-steps-container {
     width: 100%;
+    padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+}
+.step-item {
+    padding: 1rem 0;
+    display: flex;
+    border-left: 2px solid #3f51b5;
+    &.upcoming {
+        border-left: 2px solid #bdbdbd;
+    }
+}
+.left {
+    display: flex;
+    width: 75px;
+}
+.status-icon {
+    margin-left: 15px;
+    color: #3f51b5;
+}
+.step-type {
+    font-size: 16px;
+    color: #263238;
 }
 .step-name {
-    width: 100%;
-    margin-bottom: 8px;
+    margin: 4px 0;
+    font-size: 12px;
+}
+.right {
+    flex-grow: 1;
 }
 </style>
 <script>
